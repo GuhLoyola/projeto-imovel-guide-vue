70d3d4406476bfe594f34bf8c50a75c5fba78117
Gustavo Loyola <guga.loyola17@gmail.com>
2024-02-29 00:17:23 -0300

    Botão de mudar a cor alterado para o slider

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="card">
-        <button @click="changeColors" class="color-change-btn" :style="{ backgroundColor: cardBackgroundColor }">Trocar
-            cor</button>
+
+        <label class="switch">
+            <input type="checkbox" @click="changeColors">
+            <span class="slider"></span>
+        </label>
+
         <div class="card-container">
             <div class="card-img" :style="{ backgroundColor: cardBackgroundColor }">
                 <div v-if="user && user.image">
@@ -29,9 +33,61 @@
     gap: 10px;
 }
 
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ECB117;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border: 1px solid #cccccc;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 24px;
+  width: 24px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #9B0021;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #9B0021;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
 .card-container {
     border: 1px solid #cccccc;
-    box-shadow: 10px 10px 15px -7px rgba(0,0,0,0.38);
+    box-shadow: 10px 10px 15px -7px rgba(0, 0, 0, 0.38);
 }
 
 .color-change-btn {
